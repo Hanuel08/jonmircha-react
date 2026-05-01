08b8deb2013417713d4229d8583f269f008ef5bb
Hanuel08 <hanuel345martinez@gmail.com>
2026-04-30 20:43:01 -0400

    fix react-basics loading errors

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,35 +1,114 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+
+//import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Componente from "./components/Componente";
+import Propiedades from "./components/Propiedades";
+import Estado from "./components/Estado";
+import RenderizadoCondicional from "./components/RenderizadoCondicional";
+import RenderizadoElementos from "./components/RenderizadoElementos";
+import { EventosES6, EventosES7, MasSobreEventos } from "./components/Eventos";
+import ComunicacionComponentes from "./components/ComunicacionComponentes";
+import CicloVida from "./components/CicloVida";
+import AjaxApis from "./components/AjaxApis";
+// import ContadorHooks from "./components/ContadorHooks";
+// import ScrollHooks from "./components/ScrollHooks";
+// import RelojHooks from "./components/RelojHooks";
+// import AjaxHooks from "./components/AjaxHooks";
+// import HooksPersonalizados from "./components/HooksPersonalizados";
+// import Referencias from "./components/Referencias";
+// import Formularios from "./components/Formularios";
+// import Estilos from "./components/Estilos";
+// import ComponentesEstilizados from "./components/ComponentesEstilizados";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <section>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </section>
+        <section>
+          <Componente msg="Hola soy un Componente Funcional Expresado desde una prop" />
+          <hr />
+          <Propiedades
+            cadena="Esto es una cadena de texto"
+            numero={19}
+            booleano={true}
+            arreglo={[1, 2, 3]}
+            objeto={{ nombre: "Jon", correo: "jonmircha@gmail.com" }}
+            funcion={(num) => num * num}
+            elementoReact={<i>Esto es un elemento React</i>}
+            componenteReact={
+              <Componente msg="Soy un componente pasado como Prop" />
+            }
+          />
+          <hr />
+          <Estado />
+          <hr />
+          <RenderizadoCondicional />
+          <hr />
+          <RenderizadoElementos />
+          <hr />
+          <EventosES6 />
+          <hr />
+          <EventosES7 />
+          <hr />
+          <MasSobreEventos />
+          <hr />
+          <ComunicacionComponentes />
+          <hr />
+          <CicloVida />
+          <hr />
+          <AjaxApis />
+          <hr />
+          {/* <ContadorHooks titulo="Seguidores" />
+          <hr />
+          <ScrollHooks />
+          <hr />
+          <RelojHooks />
+          <hr />
+          <AjaxHooks />
+          <hr />
+          <HooksPersonalizados />
+          <hr />
+          <Referencias />
+          <hr />
+          <Formularios />
+          <hr />
+          <Estilos />
+          <hr />
+          <ComponentesEstilizados /> */}
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </section>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
